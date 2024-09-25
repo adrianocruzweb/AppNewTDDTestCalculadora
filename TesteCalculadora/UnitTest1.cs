1c798f68014cfd0a416facdfd7e90bfa320a55e3
adrianocruzweb <adrianocruzweb@hotmail.com>
2024-09-25 17:00:07 -0300

    add test methods done

--- TesteCalculadora/UnitTest1.cs
+++ TesteCalculadora/UnitTest1.cs
@@ -5,7 +5,7 @@ namespace TesteCalculadora;
 public class UnitTest1
 {
     [Fact]
-    public void TestSomar()
+    public void TestSomarSimples()
     {
         Calculadora calculadora = new Calculadora();
 
@@ -17,7 +17,7 @@ public class UnitTest1
     [Theory]
     [InlineData(1,2,3)]
     [InlineData(4,5,9)]
-    public void TestSomarNovo(int val1, int val2, int resultado)
+    public void TestSomar(int val1, int val2, int resultado)
     {
         Calculadora calculadora = new Calculadora();
 
@@ -25,4 +25,64 @@ public class UnitTest1
 
         Assert.Equal(resultado, resultadoCalculado);
     }
+
+    [Theory]
+    [InlineData(1,2,-1)]
+    [InlineData(2,1,1)]
+    public void TestSubtrair(int val1, int val2, int resultado)
+    {
+        Calculadora calculadora = new Calculadora();
+
+        int resultadoCalculado = calculadora.subtrair(val1,val2);
+
+        Assert.Equal(resultado, resultadoCalculado);
+    }
+
+    [Theory]
+    [InlineData(1,2,2)]
+    [InlineData(2,1,2)]
+    public void TestMultiplicar(int val1, int val2, int resultado)
+    {
+        Calculadora calculadora = new Calculadora();
+
+        int resultadoCalculado = calculadora.multiplicar(val1,val2);
+
+        Assert.Equal(resultado, resultadoCalculado);
+    }
+
+    [Theory]
+    [InlineData(4,2,2)]
+    [InlineData(2,1,2)]
+    public void TestDividir(int val1, int val2, int resultado)
+    {
+        Calculadora calculadora = new Calculadora();
+
+        int resultadoCalculado = calculadora.dividir(val1,val2);
+
+        Assert.Equal(resultado, resultadoCalculado);
+    }
+
+    [Fact]
+    public void TestarDivisaoPorZero()
+    {
+        Calculadora calculadora = new Calculadora();
+
+        Assert.Throws<DivideByZeroException>(() => calculadora.dividir(3,0));
+    }
+
+    /* [Fact]
+    public void TestarHistorico()
+    {
+        Calculadora calculadora = new Calculadora();
+
+        calculadora.somar(1,2);
+        calculadora.somar(2,2);
+        calculadora.somar(1,3);
+        calculadora.somar(2,3);
+
+        var lista = calculadora.historico()
+
+        Assert.NotEmpty(lista);
+        Assert.Equal(3,lista.Count());
+    } */
 }
